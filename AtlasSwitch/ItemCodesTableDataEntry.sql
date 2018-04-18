341f6edfae098bbf83ae1b8b9628432cff6faa8a
John Paradise <John.Paradise117@gmail.com>
2018-04-18 15:38:41 -0400

    ItemCode table + data entry query

---
+++ AtlasSwitch/ItemCodesTableDataEntry.sql
@@ -0,0 +1,12 @@
+﻿TRUNCATE TABLE ItemCodes;
+
+INSERT INTO ItemCodes
+VALUES
+(1, 'A Series MLO PanelBoards'),
+(2, 'A Series MCB Panelboards'),
+(3, 'Spectra Series CB MLO'),
+(4, 'Spectra Series CB MCB'),
+(5, 'Spectra Series ADS MLO'),
+(6, 'Spectra Series ADS MAIN SW'),
+(7, 'Switchboard'),
+(8, 'Transformers')
