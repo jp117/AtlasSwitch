7d60a9f37519a616893be173b583c34a293c3b05
John Paradise <John.Paradise117@gmail.com>
2018-04-19 15:46:16 -0400

    Built Main Page form, Encryption Class, and maybe finished database design

---
+++ AtlasSwitch/AtlasSwitch/AesCrypt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace AesEncDec
+{
+    class AesCrypt
+    {
+        public static string IV = "WDvfg56hjers6opj";
+        public static string KEY = "Vak9azInZ5aSvfnZYT8w4OCMqdbS3sYl";
+        public static string Encrypt(string decrypted)
+        {
+            byte[] textbytes = ASCIIEncoding.ASCII.GetBytes(decrypted);
+            AesCryptoServiceProvider encdec = new AesCryptoServiceProvider();
+            encdec.BlockSize = 128;
+            encdec.KeySize = 256;
+            encdec.Key = ASCIIEncoding.ASCII.GetBytes(KEY);
+            encdec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            encdec.Padding = PaddingMode.PKCS7;
+            encdec.Mode = CipherMode.CBC;
+
+            ICryptoTransform icrypt = encdec.CreateEncryptor(encdec.Key, encdec.IV);
+
+            byte[] enc = icrypt.TransformFinalBlock(textbytes, 0, textbytes.Length);
+            icrypt.Dispose();
+
+            return Convert.ToBase64String(enc);
+        }
+
+        public static string Decrypt(string encrypted)
+        {
+            byte[] encbytes = Convert.FromBase64String(encrypted);
+            AesCryptoServiceProvider encdec = new AesCryptoServiceProvider();
+            encdec.BlockSize = 128;
+            encdec.KeySize = 256;
+            encdec.Key = ASCIIEncoding.ASCII.GetBytes(KEY);
+            encdec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            encdec.Padding = PaddingMode.PKCS7;
+            encdec.Mode = CipherMode.CBC;
+
+            ICryptoTransform icrypt = encdec.CreateEncryptor(encdec.Key, encdec.IV);
+
+            byte[] dec = icrypt.TransformFinalBlock(encbytes, 0, encbytes.Length);
+            icrypt.Dispose();
+
+            return ASCIIEncoding.ASCII.GetString(dec);
+        }
+    }
+}
