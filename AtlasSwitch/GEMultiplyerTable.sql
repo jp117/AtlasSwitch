0cd36f2d2dea728fc10b4ec7b709ebe0e30eabc7
John Paradise <John.Paradise117@gmail.com>
2018-04-20 15:19:27 -0400

    Remade DB bc old design did not work

---
+++ AtlasSwitch/GEMultiplyerTable.sql
@@ -0,0 +1,23 @@
+﻿TRUNCATE TABLE GEMultiplyerTable;
+
+INSERT INTO GEMultiplyerTable
+VALUES
+(1, 'PNBD COMP', 6, '31', 0.1166),
+(2, 'CONTROLS & P/L', 7, 'PP P5', 0.5400),
+(3, 'CR160 Contactor', 8, 'CM', 0.5030),
+(4, 'Bus Duct Switches', 9, '79', 1.0865),
+(5, 'Heavy Duty Switches 30-200A', 10, 'S1A S1C', 0.2379),
+(6, 'Heavy Duty Switches 400 & 600A', 11, 'S1B S1D S1S', 0.2478),
+(7, 'Gen Duty Switches 30-200A', 12, 'SAA SAC', 0.2120),
+(8, 'Gen Duty Switches 400 & 600A', 13, 'SAB SAD', 0.2415),
+(9, 'QB-GFIs and AFIs', 14, 'B1', .1961),
+(10, 'Enclosures', 15, 'B2', 0.2957),
+(11, 'Pnbld Kits', 16, '3O', .0855),
+(12, 'THQB & THHQB',17, 'EBN', .1215),
+(13, 'TEY 1P 2P 3P', 18, 'EYE EYF EYG', 0.1322),
+(14, 'TEYF 1P, 2P, 3P', 19, 'EYH EYI EYJ', 0.1279),
+(15, 'TEYD 1P', 20, 'EYA', 0.1279),
+(16, 'TEYD 2P 3P', 21, 'EYB EYC', 0.1050),
+(17, 'TEYH 1P', 22, 'EYK', 0.1279),
+(18, 'TEYH 2P 3P', 23, 'EYL EYM', .0945)
+--Not including the rest.  I don't think we should include this table
